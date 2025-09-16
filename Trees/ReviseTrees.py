e12e344916f34f123e70fc8ad48c5e9149e876f2
anandita-3217 <dakshugarimella@gmail.com>
2025-09-16 23:57:15 +0530

    trying to revise trees

--- Trees/ReviseTrees.py
+++ Trees/ReviseTrees.py
@@ -1,4 +1,7 @@
 from collections import deque
+
+
+
 class TreeNode:
     def __init__(self,data):
         self.data = data
@@ -40,5 +43,83 @@ class BinaryTree:
                 queue.append(current.left)
                 queue.append(current.right)
 
+    def inorder_recursive(self, node=None):
+        if node is None:
+            node=self.root
+        result =[]
+        if node:
+            result.extend(self.inorder_traversal(node.left))
+            result.append(node.data)
+            result.extend(self.inorder_traversal(node.right))
+        return  result
+    def preorder_traversal(self,node=None):
+        if node is None:
+            node = self.root
+        result =  []
+        if node:
+            result.append(node.data)
+            result.extend(self.preorder_traversal(node.left))
+            result.extend(self.preorder_traversal(node.right))
+
+        return  result
+    def postorder_traversal(self,node=None):
+        if node is None:
+            node = self.root
+        result = []
+        if node:
+            result.extend(self.postorder_traversal(node.left))
+            result.extend(self.postorder_traversal(node.right))
+            result.append(node.data)
+        return result
+
+    def count_nodes(self,node= None):
+        if node is None:
+            node = self.root
+        if node is None:
+            return 0
+        return 1+self.count_leaf_nodes(node.left)+self.count_nodes(node.right)
+    def count_leaf_nodes(self,node = None):
+        if node is None:
+            node = self.root
+        if node is None:
+            return 0
+        if node.left is None and node.right is None:
+            return 1
+        return self.count_leaf_nodes(node.left) +self.count_nodes(node.right)
+    def height(self,node=None):
+        if node is None:
+            node = self.root
+        if node is None:
+            return  -1
+        left_height = self.height(node.left)
+        right_height = self.height(node.right)
+        return 1 + max(left_height,right_height)
+
+    def display_tree(self):
+        if self.root is None:
+            return
+        print("Tree Structure: ")
+        self._display_tree_helper(self.root,"",True)
+        print(f"Total Node: {self.get_size()}")
+        print(f"Height: {self.height()}")
+
+
+    def _display_tree_helper(self,node,prefix,is_last):
+        if node is None:
+            node = self.root
+            children = [child for child in [node.left,node.right] if child]
+            for i, child in enumerate(children):
+                is_last_child = i == len(children) - 1
+                extension = "   " if is_last else "|    "
+                self._display_tree_helper(child,prefix+extension,is_last_child)
 if __name__ == "__main__":
     print(f"Running Trees")
+    print("1. Creating Binary Tree")
+    bt = BinaryTree()
+    for i in [1,2,3,4,5,6,7]:
+        bt.insert_level_order(i)
+    bt.display_tree()
+    print("\n2. Tree Traversals:")
+    print(f"Inorder (Recursive):   {bt.inorder_recursive()}")
+    print(f"Preorder (Recursive):   {bt.preorder_traversal()}")
+    print(f"Postorder (Recursive):   {bt.postorder_traversal()}")
